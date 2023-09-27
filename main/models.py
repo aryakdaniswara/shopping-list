cb2ce8b2a4d8d779e52f7e5a7266116bf50be6d7
aryakdaniswara <arya.kdaniswara@gmail.com>
2023-09-27 13:22:39 +0700

    adding edit and delete product

--- main/models.py
+++ main/models.py
@@ -4,9 +4,9 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Product(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     name = models.CharField(max_length=255)
     date_added = models.DateField(auto_now_add=True)
     price = models.IntegerField()
-    description = models.TextField(default="")
+    description = models.TextField()
 
