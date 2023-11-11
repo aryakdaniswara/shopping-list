a5634b90f40c94600f8b6e611ed895f070dc7e2a
aryakdaniswara <arya.kdaniswara@gmail.com>
2023-11-11 12:02:56 +0700

    will this deploy

--- main/views.py
+++ main/views.py
@@ -21,7 +21,7 @@ def show_main(request):
         'class': 'PBP B', # Kelas PBP kamu
         'products': products,
         'products_count' : len(products),
-        'last_login' : request.COOKIES['last_login'],
+        # 'last_login' : request.COOKIES['last_login'],
     }
 
     return render(request, "main.html", context)
